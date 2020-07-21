138dd45ac0819877427a3156c1d5d950480244e7
Stanley Liu <sliu1993@hotmail.co.uk>
2020-07-21 23:06:14 +0100

    started building hero

---
+++ index.js
@@ -0,0 +1,21 @@
+// Compiler-included build of Vue
+import Vue from 'vue/dist/vue.esm.js';
+
+// Components
+import Hero from './components/Hero.vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+    if(document.getElementById('v-app')) {
+      Vue.prototype.$eventHub = new Vue()
+    
+      const app = new Vue({
+        el: '#v-app',
+        components: {
+            Hero
+        }
+      })
+
+      
+    }
+  })
+  
