5a73dba8a1198ebe7a187803c969021b50e50018
Stanley Liu <sliu1993@hotmail.co.uk>
2020-12-30 15:34:35 +0000

    added reactive behaviour to sidebar/navbar depending on whether home element is visible in viewport

--- src/components/Hero/HeroSidebar.vue
+++ src/components/Hero/HeroSidebar.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="hero-sidebar">
     <div class="hero-sidebar__text">
-      <template v-if="this.$route.path === '/'">
+      <template v-if="this.$route.path === '/' || homeElVisible">
         <h1>{{ name }}</h1>
         <hr />
         <h3 class="hero-sidebar__description">{{ desc }}</h3>
         <h3 class="hero-sidebar__skills">{{ skills }}</h3>
       </template>
       <template v-else>
-        <router-link to="/"><h3>Home</h3></router-link>
-        <router-link to="/projects"><h3>Projects</h3></router-link>
-        <router-link to="/about"><h3>About</h3></router-link>
-        <router-link to="/contact-me"><h3>Contact Me</h3></router-link>
+        <router-link
+          v-for="(route, index) in this.$router.options.routes"
+          :key="index"
+          :to="route.path"
+        >
+          <h3>{{ route.path | turnIntoName }}</h3>
+        </router-link>
       </template>
     </div>
 
     <div class="hero-sidebar__footer">
-      <small>Coded with <s>hatred</s>💙 in London</small>
+      <small>Coded with <s>hatred</s>💙 in London - 2020</small>
     </div>
   </div>
 </template>
@@ -24,16 +27,56 @@
 <script>
 export default {
   name: 'HeroSidebar',
-  mounted() {
-    console.log(this.$router.currentRoute)
-  },
   data() {
     return {
       name: 'Stanley Liu',
       desc: 'Full Stack Web Developer',
-      skills: 'Vue | Rails | SQL  SCSS'
+      skills: '',
+      homeElVisible: undefined
     }
-  }
+  },
+  filters: {
+    turnIntoName: function (path) {
+      if (path === "/") { return 'Home' }
+
+      if (path.match(/-/)) {
+        const pathName = path.slice(1)
+        return pathName.split('-').map((str) => {
+          return str.charAt(0).toUpperCase() + str.slice(1)
+        }).join(' ')
+      }
+      else {
+        return path.charAt(1).toUpperCase() + path.slice(2)
+      }
+    }
+  },
+  mounted() {
+    const options = {
+      rootMargin: '0px',
+      threshold: 0.5
+    }
+
+    const homeEl = document.querySelector('.container--home')
+
+    const vm = this
+
+    const callback = (entries, observer) => {
+      entries.forEach(entry => {
+        vm.homeElVisible = entry.isIntersecting
+      })
+    }
+
+    const watchHomeElem = new IntersectionObserver(callback, options)
+    watchHomeElem.observe(homeEl)
+  },
+  methods: {
+    showAndScrollToDiv(event) {
+      console.log(event.target)
+      //    const routerView = document.querySelector(".container--router");
+      //    routerView.style.display = "flex";
+      //    routerView.scrollIntoView({ behavior: "smooth" });
+    }
+  },
 }
 </script>
 
